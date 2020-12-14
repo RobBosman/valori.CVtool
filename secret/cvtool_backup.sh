4b89c8a7aa52ae894efafd5b2dd55a4a29d54f88
Rob Bosman <rob.bosman.nl+github@gmail.com>
2020-12-14 13:18:34 +0100

    - automated backups

---
+++ secret/cvtool_backup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export BACKUP_DATE=date +'%Y-%m-%d'
+
+docker exec -it \
+    "$(docker ps -aqf 'name=mongo')" \
+    mongodump --out=/backup/${BACKUP_DATE} --gzip --uri=mongodb://root:MongoPassword@mongodb:27017/cvtool?authSource=admin
+lftp -c "\
+    set ftp:ssl-allow true ;\
+    set ssl:verify-certificate no ;\
+    open -u VCV01,WYenU7hKxnet3vyD 157.97.115.138 \
+      -e \" \
+        mirror -R /var/lib/docker/volumes/root_mongodb_backup/_data/${BACKUP_DATE} /cvtool/${BACKUP_DATE} ;\
+        quit \
+        \" \
+    "
