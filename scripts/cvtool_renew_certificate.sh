8d16d2d3d4c28bdcedcebee44d39ff7621b84be7
Rob Bosman <rob.bosman.nl+github@gmail.com>
2020-12-14 22:42:32 +0100

    - automated backups

---
+++ scripts/cvtool_renew_certificate.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+docker run --rm -it \
+    --name certbot \
+    --mount source=root_ssl_certs,target=/etc/letsencrypt \
+    --mount source=root_webroot,target=/data/letsencrypt \
+    --mount source=root_letsencrypt_logs,target=/var/log/letsencrypt \
+    --mount source=root_letsencrypt_var,target=/var/lib/letsencrypt \
+    certbot/certbot \
+    renew \
+    --webroot \
+    --webroot-path=/data/letsencrypt \
+    --quiet \
+    && \
+    docker restart "$(docker ps -aqf 'ancestor=bransom/cvtool')"
