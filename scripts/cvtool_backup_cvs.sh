9b983cc76613d04b6b5adfa014980581e4e2998a
Rob Bosman <rob.bosman.nl+github@gmail.com>
2020-12-25 00:26:16 +0100

    - dots on the i

--- scripts/cvtool_backup_cvs.sh
+++ scripts/cvtool_backup_cvs.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/sh
 . /secret/.env
 
-BACKUP_DATE="$(date +'%Y-%m-%d')"\
-DOCKER_CONTAINER="$(docker ps -aqf 'ancestor=bransom/cvtool')"
+BACKUP_DATE="$(date +'%Y-%m-%d')"
+BACKUP_FILE=all-docx.zip
 
-docker exec ${DOCKER_CONTAINER} \
-    mkdir -p /backup/${BACKUP_DATE}
-
-docker exec ${DOCKER_CONTAINER} \
-    wget http://localhost:${CONTROL_PORT}/all-docx.zip -q -P /backup/${BACKUP_DATE}
+docker exec "$(docker ps -aqf 'ancestor=bransom/cvtool')" \
+    sh -c "\
+        mkdir -p /backup/${BACKUP_DATE} \
+        && rm -f /backup/${BACKUP_DATE}/${BACKUP_FILE} \
+        && wget -P /backup/${BACKUP_DATE} http://localhost:${CONTROL_PORT}/${BACKUP_FILE} \
+    "
 
 lftp -c "\
-    set ftp:ssl-allow true ;\
-    set ssl:verify-certificate no ;\
-    open -u ${LFTP_USERNAME},${LFTP_PASSWORD} ${LFTP_HOST} \
-      -e \" \
-        mirror -R /var/lib/docker/volumes/root_cvtool_backup/_data/${BACKUP_DATE} /cvtool/${BACKUP_DATE} ;\
-        quit \
-        \" \
+    set ftp:ssl-allow true \
+    ; set ssl:verify-certificate no \
+    ; open -u ${LFTP_USERNAME},${LFTP_PASSWORD} ${LFTP_HOST} \
+        -e \" \
+        mirror -R /var/lib/docker/volumes/root_cvtool_backup/_data/${BACKUP_DATE} /cvtool/${BACKUP_DATE} \
+        ; quit \
+    \" \
     "
--- scripts/cvtool_backup_mongodb.sh
+++ scripts/cvtool_backup_mongodb.sh
@@ -2,17 +2,16 @@
 . /secret/.env
 
 BACKUP_DATE="$(date +'%Y-%m-%d')"
-DOCKER_CONTAINER="$(docker ps -aqf 'name=mongo')"
 
-docker exec ${DOCKER_CONTAINER} \
+docker exec "$(docker ps -aqf 'name=mongo')" \
     mongodump --out=/backup/${BACKUP_DATE} --gzip --uri=mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@mongodb:27017/cvtool?authSource=admin
 
 lftp -c "\
-    set ftp:ssl-allow true ;\
-    set ssl:verify-certificate no ;\
-    open -u ${LFTP_USERNAME},${LFTP_PASSWORD} ${LFTP_HOST} \
-      -e \" \
-        mirror -R /var/lib/docker/volumes/root_mongodb_backup/_data/${BACKUP_DATE} /cvtool/${BACKUP_DATE} ;\
-        quit \
+    set ftp:ssl-allow true \
+    ; set ssl:verify-certificate no \
+    ; open -u ${LFTP_USERNAME},${LFTP_PASSWORD} ${LFTP_HOST} \
+        -e \" \
+            mirror -R /var/lib/docker/volumes/root_mongodb_backup/_data/${BACKUP_DATE} /cvtool/${BACKUP_DATE} \
+            ; quit \
         \" \
     "
