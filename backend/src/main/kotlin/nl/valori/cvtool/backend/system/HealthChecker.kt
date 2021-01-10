155d52ea3e0f22499101d926fc6788e5357e04d5
Rob Bosman <rob.bosman.nl+github@gmail.com>
2021-01-10 21:36:26 +0100

    - improved error logging of health check

--- backend/src/main/kotlin/nl/valori/cvtool/backend/system/HealthChecker.kt
+++ backend/src/main/kotlin/nl/valori/cvtool/backend/system/HealthChecker.kt
@@ -8,6 +8,7 @@ import nl.valori.cvtool.backend.Main
 import nl.valori.cvtool.backend.authorization.AuthenticateVerticle
 import nl.valori.cvtool.backend.persistence.MongoConnection
 import org.slf4j.LoggerFactory
+import java.util.concurrent.TimeUnit.MILLISECONDS
 
 internal object HealthChecker {
 
@@ -18,44 +19,50 @@ internal object HealthChecker {
             .create(vertx)
 
             // Check if all verticles are running.
-            .register("Verticles", 100) { healthStatus ->
-                val verticlesNotUp = Main.verticleDeploymentStates.filter { (_, state) -> state != "UP" }
+            .register("Verticles", 5_000) { healthStatus ->
+                val verticlesNotUp = Main.verticleDeploymentStates
+                    .filter { (_, state) -> state != "UP" }
                 if (verticlesNotUp.isEmpty()) {
-                    healthStatus.tryComplete(Status.OK())
+                    healthStatus.complete(Status.OK())
                 } else {
                     // Not all verticles are properly started.
                     val details = JsonObject()
-                    verticlesNotUp.forEach { (verticleClass, state) -> details.put(verticleClass.simpleName, state) }
-                    log.warn("Not all verticles are properly started. ${details.encode()}")
-                    healthStatus.tryComplete(Status.KO(details))
+                    verticlesNotUp
+                        .forEach { (verticleClass, state) ->
+                            details.put(verticleClass.simpleName, state)
+                        }
+                    log.warn("Not all verticles are up-and-running. ${details.encode()}")
+                    healthStatus.complete(Status.KO(details))
                 }
             }
 
             // Chek if MongoDB is up and running.
-            .register("MongoDB", 1_000) { healthStatus ->
+            .register("MongoDB", 5_000) { healthStatus ->
                 MongoConnection
                     .connectToDatabase(config)
+                    .timeout(2_000, MILLISECONDS)
                     .subscribe(
                         {
-                            healthStatus.tryComplete(Status.OK())
+                            healthStatus.complete(Status.OK())
                         },
                         {
                             log.warn("MongoDB is not available.", it)
-                            healthStatus.tryComplete(Status.KO(JsonObject().put("error", it.message)))
+                            healthStatus.complete(Status.KO(JsonObject().put("error", it.message)))
                         })
             }
 
             // Check if the OpenID provider is up and running.
-            .register("OpenID", 1_000) { healthStatus ->
+            .register("OpenID", 5_000) { healthStatus ->
                 AuthenticateVerticle
                     .checkOpenIdConnection()
+                    .timeout(2_000, MILLISECONDS)
                     .subscribe(
                         {
-                            healthStatus.tryComplete(Status.OK())
+                            healthStatus.complete(Status.OK())
                         },
                         {
                             log.warn("OpenID provider is not available.", it)
-                            healthStatus.tryComplete(Status.KO(JsonObject().put("error", it.message)))
+                            healthStatus.complete(Status.KO(JsonObject().put("error", it.message)))
                         })
             }
 }
