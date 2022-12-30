9b1a835913ee71a483e8c738c601ee8f5b40de66
RobBosman <Rob.Bosman@ing.com>
2022-12-30 19:20:39 +0100

    fix: frontend vulnerability update

--- scripts/cvtool_restore_mongodb_localhost.cmd
+++ scripts/cvtool_restore_mongodb_localhost.cmd
@@ -3,7 +3,7 @@
 ECHO "Make sure MongoDB is up and running."
 SET ENV_FILE=\secret\.env
 
-SET RESTORE_DATE=2022-12-20
+SET RESTORE_DATE=2022-12-29
 SET BACKUP_DIR=\backup\%RESTORE_DATE%
 SET MONGORESTORE=C:\Program Files\MongoDB\Tools\100\bin\mongorestore
 
