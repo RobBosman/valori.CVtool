2afe04bb769b7b3ddb73b400ffd3b70232b3d8aa
RobBosman <Rob.Bosman@ing.com>
2023-06-30 21:19:48 +0200

    fix: updated versions

--- cvtool-frontend/src/main/js/app.jsx
+++ cvtool-frontend/src/main/js/app.jsx
@@ -12,7 +12,7 @@ import { uiEpics } from "./services/ui/ui-epics";
 import { ErrorBoundary } from "./utils/ErrorBoundary";
 import Main from "./components/Main";
 
-export const appVersion = "versie 2023-06-05";
+export const appVersion = "versie 2023-07-01";
 
 epicRegistry.register(
   ...errorEpics,
