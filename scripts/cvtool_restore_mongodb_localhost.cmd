a7c8b5d5f3598b6acdf82f322b205ac297aec635
RobBosman <rob.bosman.nl+github@gmail.com>
2023-08-05 17:34:13 +0200

    fix: minor improvements

--- scripts/cvtool_restore_mongodb_localhost.cmd
+++ scripts/cvtool_restore_mongodb_localhost.cmd
@@ -3,7 +3,7 @@
 ECHO "Make sure MongoDB is up and running."
 SET ENV_FILE=\secret\.env
 
-SET RESTORE_DATE=2023-07-27
+SET RESTORE_DATE=2023-08-04
 SET BACKUP_DIR=\backup\%RESTORE_DATE%
 SET MONGORESTORE=C:\Program Files\MongoDB\Tools\100\bin\mongorestore
 
