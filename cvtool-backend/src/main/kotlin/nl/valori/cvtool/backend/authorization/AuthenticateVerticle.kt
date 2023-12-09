37289bf44d12d04f12b1c7f65edf6c48737ef789
RobBosman <rob.bosman.nl+github@gmail.com>
2023-12-09 19:08:04 +0100

    fix: attempt to fix OpenID SSL issue

--- cvtool-backend/src/main/kotlin/nl/valori/cvtool/backend/authorization/AuthenticateVerticle.kt
+++ cvtool-backend/src/main/kotlin/nl/valori/cvtool/backend/authorization/AuthenticateVerticle.kt
@@ -3,6 +3,8 @@ package nl.valori.cvtool.backend.authorization
 import io.reactivex.Single
 import io.reactivex.exceptions.CompositeException
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.ReplaySubject
+import io.reactivex.subjects.Subject
 import io.vertx.core.Promise
 import io.vertx.core.eventbus.ReplyFailure.RECIPIENT_FAILURE
 import io.vertx.core.json.JsonObject
@@ -16,7 +18,7 @@ import io.vertx.reactivex.ext.auth.oauth2.providers.OpenIDConnectAuth
 import org.slf4j.LoggerFactory
 import java.net.URI
 import java.util.concurrent.TimeUnit.MILLISECONDS
-import java.util.function.BiConsumer
+import java.util.function.Consumer
 
 const val AUTHENTICATE_ADDRESS = "authenticate"
 const val AUTHENTICATE_HEALTH_ADDRESS = "authenticate.health"
@@ -25,33 +27,22 @@ const val AUTH_DOMAIN = "Valori.nl"
 internal class AuthenticateVerticle : AbstractVerticle() {
 
     private val log = LoggerFactory.getLogger(AuthenticateVerticle::class.java)
+    private val oauth2Subject: Subject<OAuth2Auth> = ReplaySubject.create(1)
+    private val oauth2SslTimeoutMillis = 10_000L
+    private val oauth2RetryAfterMillis = 5_000L
+    private val oauth2RefreshIntervalMillis = 2 * 60 * 1000L
 
     override fun start(startPromise: Promise<Void>) { //NOSONAR - Promise<Void> is defined in AbstractVerticle
         // Configure the connection to the OpenId Provider and refresh it regularly.
-        // Environment variable:
-        //   AUTH_CONNECTION_STRING=<OPENID_PROVIDER_URL>/<TENANT_ID>/v2.0?<CLIENT_ID>:<CLIENT_SECRET>
-        val connectionString = config().getString("AUTH_CONNECTION_STRING")
-        val clientIdAndSecret = URI(connectionString).query.split(":")
-        val oauth2Options = OAuth2Options()
-            .setSite(connectionString.substringBefore("?"))
-            .setClientId(clientIdAndSecret[0])
-            .setClientSecret(clientIdAndSecret[1])
-        // Prevent SSL handshake timeouts. Especially when establishing remote connections from virtual environments.
-        oauth2Options.httpClientOptions
-            .setSslHandshakeTimeoutUnit(MILLISECONDS)
-            .setSslHandshakeTimeout(20_000)
+        initOauth2Connection(config())
 
-        // Obtain a connection to the OpenID Provider.
-        OpenIDConnectAuth
-            .rxDiscover(vertx, oauth2Options)
-            .observeOn(Schedulers.io())
-            .doOnError { log.warn("Error connecting to OpenID Provider: ${it.message}", it) }
-            .retryWhen { it.delay(5_000, MILLISECONDS) }
+        // Wait for Oauth2 to connect and then finalize startup.
+        oauth2Single()
             .subscribe(
                 {
                     // Provide the connection to the vertx handlers.
-                    handleVertxEvents(AUTHENTICATE_ADDRESS, ::handleAuthenticationRequest, it)
-                    handleVertxEvents(AUTHENTICATE_HEALTH_ADDRESS, ::handleHealthRequest, it)
+                    handleVertxEvents(AUTHENTICATE_ADDRESS, ::handleAuthenticationRequest)
+                    handleVertxEvents(AUTHENTICATE_HEALTH_ADDRESS, ::handleHealthRequest)
 
                     startPromise.tryComplete()
                     log.info("Successfully configured the connection to OpenID Provider")
@@ -63,17 +54,49 @@ internal class AuthenticateVerticle : AbstractVerticle() {
             )
     }
 
+    private fun initOauth2Connection(config: JsonObject) {
+        // Environment variable:
+        //   AUTH_CONNECTION_STRING=<OPENID_PROVIDER_URL>/<TENANT_ID>/v2.0?<CLIENT_ID>:<CLIENT_SECRET>
+        val connectionString = config.getString("AUTH_CONNECTION_STRING")
+        val clientIdAndSecret = URI(connectionString).query.split(":")
+        val oauth2Options = OAuth2Options()
+            .setSite(connectionString.substringBefore("?"))
+            .setClientId(clientIdAndSecret[0])
+            .setClientSecret(clientIdAndSecret[1])
+        // Prevent SSL handshake timeouts, especially when establishing remote connections from virtual environments.
+        oauth2Options.httpClientOptions
+            .setSslHandshakeTimeout(oauth2SslTimeoutMillis)
+            .setSslHandshakeTimeoutUnit(MILLISECONDS)
+
+        // Obtain a connection to the OpenID Provider.
+        OpenIDConnectAuth
+            .rxDiscover(vertx, oauth2Options)
+            .observeOn(Schedulers.io())
+            .doOnError { log.warn("Error connecting to OpenID Provider: ${it.message}", it) }
+            .retryWhen { it.delay(oauth2RetryAfterMillis, MILLISECONDS) } // Keep retrying on error.
+            .repeatWhen { it.delay(oauth2RefreshIntervalMillis, MILLISECONDS) } // Refresh regularly.
+            .subscribe(
+                {
+                    oauth2Subject.onNext(it)
+                    log.info("Refreshed connection to OpenID Provider")
+                },
+                {
+                    log.error("Cannot connect to OpenID Provider: ${it.message}", it)
+                }
+            )
+
+    }
+
     private fun handleVertxEvents(
         eventAddress: String,
-        handler: BiConsumer<Message<JsonObject>, OAuth2Auth>,
-        oauth2: OAuth2Auth
+        handler: Consumer<Message<JsonObject>>
     ) =
         vertx.eventBus()
             .consumer<JsonObject>(eventAddress)
             .toFlowable()
             .subscribe(
                 {
-                    handler.accept(it, oauth2)
+                    handler.accept(it)
                 },
                 {
                     log.error("Vertx error processing authentication request.", it)
@@ -93,13 +116,13 @@ internal class AuthenticateVerticle : AbstractVerticle() {
      *   }
      *
      */
-    private fun handleAuthenticationRequest(message: Message<JsonObject>, oauth2: OAuth2Auth) =
+    private fun handleAuthenticationRequest(message: Message<JsonObject>) =
         Single
             .just(
                 message.body().getString("jwt")
                     ?: error("Cannot obtain 'jwt' from message body.")
             )
-            .flatMap { authenticateJwt(it, oauth2) }
+            .flatMap { authenticateJwt(it) }
             .subscribe(
                 {
                     log.debug("Authenticated successfully.")
@@ -112,9 +135,9 @@ internal class AuthenticateVerticle : AbstractVerticle() {
                 }
             )
 
-    private fun authenticateJwt(jwt: String, oauth2: OAuth2Auth) =
-        oauth2
-            .rxAuthenticate(TokenCredentials(jwt))
+    private fun authenticateJwt(jwt: String) =
+        oauth2Single()
+            .flatMap { it.rxAuthenticate(TokenCredentials(jwt)) }
             .map {
                 val accessToken = it.attributes().getJsonObject("accessToken")
                 val email = accessToken.getString("preferred_username", "")
@@ -130,11 +153,16 @@ internal class AuthenticateVerticle : AbstractVerticle() {
                     .put("name", name)
             }
 
-    private fun handleHealthRequest(message: Message<JsonObject>, oauth2: OAuth2Auth) =
-        oauth2
+    private fun oauth2Single() =
+        oauth2Subject
+            .take(1)
+            .singleOrError()
+
+    private fun handleHealthRequest(message: Message<JsonObject>) =
+        oauth2Single()
             // Send an invalid authorization request to the OpenID Provider.
             // The OpenID Provider will respond with an error and thus 'prove' that the connection is still OK.
-            .rxAuthenticate(UsernamePasswordCredentials("DUMMY", "no-secret"))
+            .flatMap { it.rxAuthenticate(UsernamePasswordCredentials("DUMMY", "no-secret")) }
             .map { "" }
             .onErrorReturn {
                 if (it.message?.contains("invalid_request") != true)
