4ac89a30f2be2992205effc7e2e8045509508b02
RobBosman <rob.bosman.nl+github@gmail.com>
2023-12-09 23:59:25 +0100

    fix: attempt to fix OpenID SSL issue

--- cvtool-backend/src/main/kotlin/nl/valori/cvtool/backend/authorization/AuthenticateVerticle.kt
+++ cvtool-backend/src/main/kotlin/nl/valori/cvtool/backend/authorization/AuthenticateVerticle.kt
@@ -69,8 +69,8 @@ internal class AuthenticateVerticle : AbstractVerticle() {
             .setSslHandshakeTimeout(oauth2SslTimeoutMillis)
             .setSslHandshakeTimeoutUnit(MILLISECONDS)
 //            .setConnectTimeout(oauth2ConnectTimeoutMillis)
-            .setKeepAlive(false)
-            .setTcpKeepAlive(false)
+//            .setKeepAlive(false)
+//            .setTcpKeepAlive(false)
 
         // Obtain a connection to the OpenID Provider.
         OpenIDConnectAuth
