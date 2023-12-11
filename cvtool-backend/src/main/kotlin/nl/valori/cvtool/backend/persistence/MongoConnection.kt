f482e76af9a3a44fb043ae53ea06d1a9bb662f2b
RobBosman <rob.bosman.nl+github@gmail.com>
2023-12-11 22:39:08 +0100

    fix: adding Docker container restart command

--- cvtool-backend/src/main/kotlin/nl/valori/cvtool/backend/persistence/MongoConnection.kt
+++ cvtool-backend/src/main/kotlin/nl/valori/cvtool/backend/persistence/MongoConnection.kt
@@ -28,10 +28,10 @@ object MongoConnection {
                 {
                     mongodbSubject.onNext(it)
                     mongodbSubject.onComplete()
-                    log.info("Successfully configured the connection to MongoDB")
+                    log.info("Successfully configured MongoDB")
                 },
                 {
-                    log.error("Error configuring the connection to MongoDB", it)
+                    log.error("Error configuring MongoDB", it)
                 }
             )
     }
--- cvtool-backend/src/main/kotlin/nl/valori/cvtool/backend/system/ControlVerticle.kt
+++ cvtool-backend/src/main/kotlin/nl/valori/cvtool/backend/system/ControlVerticle.kt
@@ -11,6 +11,7 @@ import io.vertx.reactivex.core.buffer.Buffer
 import io.vertx.reactivex.ext.web.Router
 import nl.valori.cvtool.backend.cv.ALL_CVS_GENERATE_ADDRESS
 import org.slf4j.LoggerFactory
+import java.lang.Runtime.getRuntime
 import java.net.HttpURLConnection.HTTP_INTERNAL_ERROR
 import java.net.HttpURLConnection.HTTP_OK
 import java.net.URI
@@ -73,6 +74,23 @@ internal class ControlVerticle : AbstractVerticle() {
                                 .end()
                         }
                     )
+            }
+
+        router
+            .route("/restart-backend")
+            .handler { context ->
+                log.warn("Restarting Docker container 'bransom/cvtool-backend'...")
+                getRuntime().exec(
+                    arrayOf(
+                        "docker",
+                        "container",
+                        "restart",
+                        "\"$(docker ps -aqf 'ancestor=bransom/cvtool-backend')\""
+                    )
+                )
+                context.response()
+                    .setStatusCode(HTTP_OK)
+                    .end()
             }
 
         return router
