2f756a3bcaf50937544a282bbf3debd7a74a0dab
RobBosman <rob.bosman.nl+github@gmail.com>
2025-11-21 16:51:46 +0100

    fix: updated backup

--- scripts/cvtool_restore_mongodb.sh
+++ scripts/cvtool_restore_mongodb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 . /secret/.env
 
-RESTORE_DATE="2025-11-14"
+RESTORE_DATE="2025-11-20"
 
 docker exec "$(docker ps -aqf 'name=mongodb')" \
   mongorestore --uri="mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@mongodb:27017/cvtool?authSource=admin" \
--- scripts/cvtool_restore_mongodb_localhost.cmd
+++ scripts/cvtool_restore_mongodb_localhost.cmd
@@ -3,7 +3,7 @@
 ECHO "Make sure MongoDB is up and running."
 SET ENV_FILE=\secret\.env
 
-SET RESTORE_DATE=2025-11-14
+SET RESTORE_DATE=2025-11-20
 SET BACKUP_DIR=\backup\%RESTORE_DATE%
 SET MONGORESTORE=C:\Program Files\MongoDB\Tools\bin\mongorestore
 
