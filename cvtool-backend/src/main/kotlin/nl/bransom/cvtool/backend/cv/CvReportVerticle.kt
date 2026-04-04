91adbdeae6155aa2d6d6a659dd177e5de44c2773
RobBosman <rob.bosman.nl+github@gmail.com>
2026-04-05 01:08:23 +0200

    fix: cv report - WIP

--- cvtool-backend/src/main/kotlin/nl/bransom/cvtool/backend/cv/CvReportVerticle.kt
+++ cvtool-backend/src/main/kotlin/nl/bransom/cvtool/backend/cv/CvReportVerticle.kt
@@ -5,7 +5,12 @@ import io.vertx.core.eventbus.ReplyFailure.RECIPIENT_FAILURE
 import io.vertx.core.json.JsonObject
 import io.vertx.reactivex.core.eventbus.Message
 import nl.bransom.cvtool.backend.BasicVerticle
+import nl.bransom.cvtool.backend.ModelUtils.getInstances
 import nl.bransom.cvtool.backend.ModelUtils.toJsonObject
+import nl.bransom.cvtool.backend.authorization.AuthInfo
+import nl.bransom.cvtool.backend.authorization.AuthInfo.Companion.toAuthInfo
+import nl.bransom.cvtool.backend.authorization.AuthorizationLevel.ADMIN
+import nl.bransom.cvtool.backend.authorization.AuthorizationLevel.UNIT_LEAD
 import nl.bransom.cvtool.backend.persistence.MONGODB_FETCH_ADDRESS
 import nl.bransom.cvtool.backend.persistence.MONGODB_QUERY_ADDRESS
 
@@ -25,8 +30,10 @@ internal class CvReportVerticle : BasicVerticle(CV_REPORT_ADDRESS) {
     override fun handleRequest(message: Message<JsonObject>) {
         Single
             .just(message)
-            .flatMap { fetchAllAccounts() }
-            .flatMap { allAccounts -> mergeLatestAuditLogs(allAccounts) }
+            .map { JsonObject(it.headers()["authInfo"] ?: "{}").toAuthInfo() }
+            .flatMap(::fetchRequiredEntities)
+            .flatMap(::withLatestAuditLogs)
+            .map { (allEntities, auditLogs) -> merge(allEntities, auditLogs) }
             .subscribe(
                 {
                     log.info("Successfully fetched cv report")
@@ -40,16 +47,27 @@ internal class CvReportVerticle : BasicVerticle(CV_REPORT_ADDRESS) {
             )
     }
 
-    private fun fetchAllAccounts(): Single<JsonObject> =
-        vertx.eventBus()
+    private fun fetchRequiredEntities(authInfo: AuthInfo): Single<JsonObject> {
+        val businessUnitCriteria = when {
+            authInfo.isAuthorized(ADMIN) -> "{}"
+            authInfo.isAuthorized(UNIT_LEAD) -> """{ "accountIds": "${authInfo.accountId}" }"""
+            else -> return Single.just(JsonObject())
+        }
+        return vertx.eventBus()
             .rxRequest<JsonObject>(
                 MONGODB_FETCH_ADDRESS,
-                JsonObject("""{ "account": [{}] }"""),
+                JsonObject(
+                    """{
+                        "account": [{}],
+                        "businessUnit": [$businessUnitCriteria]
+                    }"""
+                ),
                 deliveryOptions
             )
-            .map { it.body().getJsonObject("account") }
+            .map { it.body() }
+    }
 
-    private fun mergeLatestAuditLogs(allAccounts: JsonObject): Single<JsonObject> =
+    private fun withLatestAuditLogs(allEntities: JsonObject): Single<Pair<JsonObject, JsonObject>> =
         vertx.eventBus()
             .rxRequest<JsonObject>(
                 MONGODB_QUERY_ADDRESS,
@@ -69,19 +87,28 @@ internal class CvReportVerticle : BasicVerticle(CV_REPORT_ADDRESS) {
                 ),
                 deliveryOptions
             )
-            .map { merge(allAccounts, it.body()) }
+            .map { allEntities to it.body() }
 
-    private fun merge(allAccounts: JsonObject, auditTimestamps: JsonObject): JsonObject {
-        return allAccounts
-            .map { (id, account) ->
-                JsonObject(
-                    """{
-                        "$id": {
-                            "name": "${toJsonObject(account)?.getString("name")}",
-                            "timestamp": "${toJsonObject(auditTimestamps.map[id])?.getString("latestTimestamp")}"
-                        }
-                    }"""
-                )
+    private fun merge(allEntities: JsonObject, auditTimestamps: JsonObject): JsonObject {
+        return allEntities.getInstances("businessUnit")
+            .flatMap { businessUnit ->
+                val accountIds = businessUnit.getJsonArray("accountIds")
+                val accounts = allEntities.getInstances("account")
+                    .filter { it.getString("_id") in accountIds }
+                accounts
+                    .map { account ->
+                        val accountId = account.getString("_id")
+                        val latestTimestamp = toJsonObject(auditTimestamps.map[accountId])?.getString("latestTimestamp")
+                        JsonObject(
+                            """{
+                                "$accountId": {
+                                    "businessUnit": "${businessUnit.getString("name")}",
+                                    "name": "${toJsonObject(account)?.getString("name")}",
+                                    "timestamp": "$latestTimestamp"
+                                }
+                            }"""
+                        )
+                    }
             }
             .reduce { a, b -> a.mergeIn(b) }
     }
