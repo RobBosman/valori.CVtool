d06460d85510415d20caeba77406076c90e33582
RobBosman <rob.bosman.nl+github@gmail.com>
2026-08-08 21:24:46 +0200

    fix: updated dependencies

--- cvtool-frontend/src/main/js/app.jsx
+++ cvtool-frontend/src/main/js/app.jsx
@@ -23,7 +23,7 @@ const htmlRoot = document.getElementsByTagName("html")[0];
 htmlRoot.style.border = dtap.borderStyle;
 htmlRoot.style.height = dtap.borderStyle ? "calc(100% - 12px)" : "100%";
 
-export const appVersion = `${dtap.label} versie 2026-08-01`;
+export const appVersion = `${dtap.label} versie 2026-08-08`;
 
 epicRegistry.register(
   ...errorEpics,
