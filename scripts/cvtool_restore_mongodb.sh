05d1afc9fca40066a7d878e69f82ed83cb1eb648
RobBosman <rob.bosman.nl+github@gmail.com>
2026-09-02 20:06:16 +0200

    fix: updated dependencies and backup

--- scripts/cvtool_restore_mongodb.sh
+++ scripts/cvtool_restore_mongodb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 . /secret/.env
 
-RESTORE_DATE="2026-08-20"
+RESTORE_DATE="2026-09-01"
 
 docker exec "$(docker ps -aqf 'name=mongodb')" \
   mongorestore --uri="mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@mongodb:27017/cvtool?authSource=admin" \
