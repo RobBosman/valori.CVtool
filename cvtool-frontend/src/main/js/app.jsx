31d861506b8789d459a041d661d5db74a5cd938d
RobBosman <rob.bosman.nl+github@gmail.com>
2026-09-12 12:26:39 +0200

    fix: updated backup

--- cvtool-frontend/src/main/js/app.jsx
+++ cvtool-frontend/src/main/js/app.jsx
@@ -23,7 +23,7 @@ const htmlRoot = document.getElementsByTagName("html")[0];
 htmlRoot.style.border = dtap.borderStyle;
 htmlRoot.style.height = dtap.borderStyle ? "calc(100% - 12px)" : "100%";
 
-export const appVersion = `${dtap.label} versie 2026-09-11`;
+export const appVersion = `${dtap.label} versie 2026-09-12`;
 
 epicRegistry.register(
   ...errorEpics,
